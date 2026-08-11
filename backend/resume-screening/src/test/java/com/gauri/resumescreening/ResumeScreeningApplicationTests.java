93a18450c070d994f682f94fcf77bbd0733af348
gauri1440 <gaurisingh1440@gmail.com>
2026-08-11 12:49:34 +0000

    Create Spring Boot backend and test API

---
+++ backend/resume-screening/src/test/java/com/gauri/resumescreening/ResumeScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauri.resumescreening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
